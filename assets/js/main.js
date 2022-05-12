767dab370203266fea1c612d5f27c956ca22c79d
psiegmund <patrick-siegmund@web.de>
2022-05-12 09:32:23 +0200

    done

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let email = document.getElementById('email');
+let overlay = document.getElementById('overlayConfirm');
+
+function showForm() {
+    overlay.style.visibility = "visible";
+}
+
+function hideForm() {
+    overlay.style.visibility = "hidden";
+}
